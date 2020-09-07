fb3a33418087d0a66bc4fc5ae9692f89ac24d8b0
MrPeaceDuke <daemonica120@yandex.ru>
2020-09-07 20:11:25 +0500

    main

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            string s = Convert.ToString(a, 2);
+            int count1 = s.ToCharArray().Where(c => c == '1').Count();
+            int count0 = s.ToCharArray().Where(c => c == '0').Count();
+            string ss = "";
+            for (int i = 0; i < count1; i++) ss += "1";
+            for (int i = 0; i < count0; i++) ss += "0";
+            Console.WriteLine(Convert.ToInt32(ss, 2));
+            Console.ReadLine();
+        }
+    }
+}
